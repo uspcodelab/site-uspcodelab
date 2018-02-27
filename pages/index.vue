9015a40395385177bdf76a42b699fdc38685ef72
Leonardo Lana <leolanavo@gmail.com>
2018-02-27 00:00:07 -0300

    Change the banner frame style
    Change the banner frame to use a wave style at the bottom border,
    instead of a rectangular frame.
    
    Signed-off-by: Leonardo Lana <leolanavo@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,56 +1,67 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        USPCodeLab
-      </h1>
-      <h2 class="subtitle">
-        Website for the extension group USPCodeLab
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+  <div class="container">
+    <nav class="navbar">
+      <div class="nav-logo">USPCodeLab</div>
+      <div class="nav-menu">
+        <div class="nav-button"> <span class="nav-button-text">Home</span> </div>
+        <div class="nav-button"> Sobre </div>
+        <div class="nav-button"> HackathonUSP </div>
+        <div class="nav-button"> dev.start() </div>
+        <div class="nav-button"> Schools </div>
+        <div class="nav-button"> Contato </div>
+      </div>
+    </nav>
+
+    <section>
+      <div class="banner"/>
+      <svg id="curve" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1416.99 174.01">
+        <path class="cls-1" d="M0,280.8S283.66,59,608.94,163.56s437.93,150.57,808,10.34V309.54H0V280.8Z" transform="translate(0 -135.53)"/>
+      </svg>
+    </section>
+
+    <div class="info-section">
+      <div class="info-title">
+        O que oferecemos
+      </div>
+
+      <div class="info">
+        <div class="info-text">
+          Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?
+        </div>
+      </div>
+
+      <div class="info">
+        <div class="info-text">
+          Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?
+        </div>
+      </div>
+
+      <div class="info">
+        <div class="info-text">
+          Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?
+        </div>
       </div>
     </div>
-  </section>
+
+    <div class="photo-menu">
+      <div class="photo-menu-entry">
+        <img src="../images/blue-glass.jpg">
+      </div>
+      <div class="photo-menu-entry">
+        <img src="../images/green-glass.jpg">
+      </div>
+      <div class="photo-menu-entry">
+        <img src="../images/blue-glass.jpg">
+      </div>
+      <div class="photo-menu-entry">
+        <img src="../images/green-glass.jpg">
+      </div>
+    </div>
+
+    <footer>
+      <div class="footer-info">a</div>
+      <div class="footer-copyright">b</div>
+    </footer>
+
+  </div>
 </template>
-
-<script>
-import Logo from "~/components/Logo.vue";
-
-export default {
-  components: {
-    Logo
-  }
-};
-</script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links {
-  padding-top: 15px;
-}
-</style>
