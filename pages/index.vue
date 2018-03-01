420741cc40182eaab59e5a56ca991510e082e76f
Leonardo Lana <leolanavo@gmail.com>
2018-03-01 10:07:37 -0300

    Modularize the components of the page
    Use `v-for` for avoiding repetition among components of
    the index.vue page.
    
    Signed-off-by: Leonardo Lana <leolanavo@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="container">
+
     <nav class="navbar">
       <div class="nav-logo">USPCodeLab</div>
       <div class="nav-menu">
-        <div class="nav-button"> <span class="nav-button-text">Home</span> </div>
-        <div class="nav-button"> Sobre </div>
-        <div class="nav-button"> HackathonUSP </div>
-        <div class="nav-button"> dev.path</div>
-        <div class="nav-button"> CodeSchools </div>
-        <div class="nav-button"> Contato </div>
-      </div>
+        <div v-for="btn in nav_buttons" :key="btn.KEY" class="nav-button">
+          <div class="nav-text">{{ btn.text }}</div>
+          <div class="nav-line"/>
+        </div>
+        </div>
     </nav>
 
     <header class="header">
       <div class="header-background"/>
-      <h1 class="title">Header Content</h1>
+        <h1 class="title">Header Content</h1>
+        <div class="logo"/>
     </header>
 
 
@@ -23,44 +23,58 @@
         O que oferecemos
       </div>
 
-      <div class="info">
+      <div v-for="info in infos" :key="info.KEY" class="info">
         <div class="info-text">
-          Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?
+          {{ info.text }}
         </div>
       </div>
 
-      <div class="info">
-        <div class="info-text">
-          Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?
+      <div class="photo-menu">
+        <div class="photo-menu-entry">
+          <img src="../images/blue-glass.jpg">
+        </div>
+        <div class="photo-menu-entry">
+          <img src="../images/green-glass.jpg">
+        </div>
+        <div class="photo-menu-entry">
+          <img src="../images/blue-glass.jpg">
+        </div>
+        <div class="photo-menu-entry">
+          <img src="../images/green-glass.jpg">
         </div>
       </div>
 
-      <div class="info">
-        <div class="info-text">
-          Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?
-        </div>
-      </div>
+      <footer>
+        <div class="footer-info">a</div>
+        <div class="footer-copyright">b</div>
+      </footer>
+
     </div>
-
-    <div class="photo-menu">
-      <div class="photo-menu-entry">
-        <img src="../images/blue-glass.jpg">
-      </div>
-      <div class="photo-menu-entry">
-        <img src="../images/green-glass.jpg">
-      </div>
-      <div class="photo-menu-entry">
-        <img src="../images/blue-glass.jpg">
-      </div>
-      <div class="photo-menu-entry">
-        <img src="../images/green-glass.jpg">
-      </div>
-    </div>
-
-    <footer>
-      <div class="footer-info">a</div>
-      <div class="footer-copyright">b</div>
-    </footer>
-
-  </div>
 </template>
+
+
+<script>
+export default {
+  data: () => {
+    return {
+      nav_buttons: [
+        { KEY: 1, text: "Home" },
+        { KEY: 2, text: "Sobre" },
+        { KEY: 3, text: "HackathonUSP" },
+        { KEY: 4, text: "dev.journey" },
+        { KEY: 5, text: "CodeSchools" },
+        { KEY: 6, text: "Contato" }
+      ],
+      infos: [
+        { KEY: 1,
+          text: "Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?"},
+        { KEY: 2,
+          text: "Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?"},
+        { KEY: 3,
+          text: "Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?"},
+
+      ]
+    };
+  }
+};
+</script>
