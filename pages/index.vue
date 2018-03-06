3914fa8dc470283d0ee77d5510d603220bd39d64
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-03-06 19:25:05 -0300

    New sections layout
    Change layouts for HackathonUSP and CodeSchools sections and create
    .scss files to style them.
    
    Import new style files in main.scss and move css class "info-title" into
    main.scss with a title of "section-title", so that it can be better used
    in all three sections.
    
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -23,27 +23,37 @@
         </div>
       </div>
 
-      <div class="info-section" id="hackathonusp">
-        <div class="info-title">
+      <div class="hackathon-usp">
+        <div class="section-title">
           <h3>HackathonUSP</h3>
         </div>
-
-        <div class="info" v-for="obj in hackathon" :key="obj.KEY" >
-          <div class="info-text">
-            {{ obj.text }}
-          </div>
+        <div class="hackathon-usp-info">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam delectus aut accusamus hic architecto sapiente tempore deserunt eos, veniam incidunt, aliquam temporibus error, odit laudantium laboriosam quia dignissimos ab ullam.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde tenetur commodi, reiciendis explicabo laudantium necessitatibus expedita ipsa in, eum sequi voluptas architecto placeat cupiditate, saepe omnis facere dignissimos provident minus.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam, sunt impedit ab maiores, animi qui ut dolorum quis optio cumque iusto magni fuga tempore similique. Maiores id expedita perferendis repudiandae?
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi totam doloribus mollitia rem eos facere, commodi animi! Quae eveniet ad, omnis suscipit doloribus quidem iusto, quod eligendi eaque adipisci aliquam?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure officia suscipit blanditiis culpa alias vel est, eum dolor, eaque, dicta debitis aspernatur aliquid sit exercitationem. Commodi possimus eveniet magnam modi?
+        </div>
+        <div class="hackathon-logo">
+          Logo placeholder
         </div>
       </div>
 
-      <div class="info-section" id="hackathonusp">
-        <div class="info-title">
-          <h3>Code Schools</h3>
+      <div class="codeschools">
+        <div class="section-title">
+          <h3>Code Schoools</h3>
         </div>
 
-        <div class="info" v-for="obj in hackathon" :key="obj.KEY" >
-          <div class="info-text">
-            {{ obj.text }}
-          </div>
+        <div class="codeschools-photos">
+          Photo gallery placeholder
+        </div>
+
+        <div class="codeschools-info">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam delectus aut accusamus hic architecto sapiente tempore deserunt eos, veniam incidunt, aliquam temporibus error, odit laudantium laboriosam quia dignissimos ab ullam.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde tenetur commodi, reiciendis explicabo laudantium necessitatibus expedita ipsa in, eum sequi voluptas architecto placeat cupiditate, saepe omnis facere dignissimos provident minus.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam, sunt impedit ab maiores, animi qui ut dolorum quis optio cumque iusto magni fuga tempore similique. Maiores id expedita perferendis repudiandae?
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi totam doloribus mollitia rem eos facere, commodi animi! Quae eveniet ad, omnis suscipit doloribus quidem iusto, quod eligendi eaque adipisci aliquam?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure officia suscipit blanditiis culpa alias vel est, eum dolor, eaque, dicta debitis aspernatur aliquid sit exercitationem. Commodi possimus eveniet magnam modi?
         </div>
       </div>
 
@@ -66,50 +76,6 @@ export default {
   data: () => {
     return {
       dev_section: [
-        {
-          KEY: 1,
-          text:
-            "Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?"
-        },
-        {
-          KEY: 2,
-          text:
-            "Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?"
-        },
-        {
-          KEY: 3,
-          text:
-            "Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?"
-        },
-        {
-          KEY: 4,
-          text:
-            "Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?"
-        }
-      ],
-      hackathon: [
-        {
-          KEY: 1,
-          text:
-            "Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?"
-        },
-        {
-          KEY: 2,
-          text:
-            "Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?"
-        },
-        {
-          KEY: 3,
-          text:
-            "Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?"
-        },
-        {
-          KEY: 4,
-          text:
-            "Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?"
-        }
-      ],
-      code_schools: [
         {
           KEY: 1,
           text:
