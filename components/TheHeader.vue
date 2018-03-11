32e15183d58940582ec5139ba700c2bd8d720689
Leonardo Lana <leolanavo@gmail.com>
2018-03-11 18:17:55 -0300

    Remove commented line
    Signed-off-by: Leonardo Lana <leolanavo@gmail.com>

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,7 +1,6 @@
 <template>
   <header id="head" class="header">
     <div class="header-background"/>
-    <!-- <h1 class="title">USPCodeLab</h1> -->
     <img src="~/assets/UCL-Logo-Text.png"
          style="z-index: 9; background-color: #ffffff; border-radius: 15px">
   </header>
