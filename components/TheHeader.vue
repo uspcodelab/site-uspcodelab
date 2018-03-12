688145c4770d36add02fba5700ad6431921efcdf
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-03-11 22:45:55 -0300

    Start to make header responsive
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,21 +1,37 @@
 <template>
   <header id="head" class="header">
     <div class="header-background"/>
-    <img src="~/assets/UCL-Logo-Text.png"
-         style="z-index: 9; background-color: #ffffff; border-radius: 15px">
+    <img src="~/assets/UCL-Logo_Text.svg" class="header-logo">
+    <div style="z-index: 9; width: 90%">
+      <img src="~/assets/UCL-Logo.svg" class="mobile-logo">
+    </div>
+    
   </header>
 </template>
 
 <style lang="scss">
+.header-logo {
+  z-index: 9;
+  background-color: #ffffff;
+  border-radius: 15px;
+  width: 65%;
+}
+.mobile-logo {
+  z-index: 9;
+  width: 100%;
+  border-radius: 25%;
+  display: none;
+}
 .header {
   position: relative;
   height: 100vh;
   overflow: hidden;
+  padding-top: 9vh;
 
   display: flex;
   flex-flow: row wrap;
-  justify-content: space-around;
   align-items: center;
+  justify-content: center;
 
   border-bottom: 10em;
 }
@@ -31,5 +47,14 @@
   background-image: linear-gradient(#ff8b46 0%, $main-color 100%);
   transform: skewY(-6deg);
   transform-origin: top left;
+}
+
+@media only screen and (max-device-width: 480px) {
+  .mobile-logo {
+    display: block;
+  }
+  .header-logo {
+    display: none;
+  }
 }
 </style>
