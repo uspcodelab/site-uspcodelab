064ec898a57ed500eb211c1bf2ed72774a8b36c3
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-03-13 20:03:18 -0300

    Make HackathonUSP section responsive
    Section about HackathonUSP is responsive, although it is not done
    because we don't have a translucid background image.
    
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- components/AboutHackathonUsp.vue
+++ components/AboutHackathonUsp.vue
@@ -6,7 +6,7 @@
       </div>
       O HackathonUSP é o nosso maior evento. De âmbito USPiano, feito em parceira com a univerdade e focado em quem nunca participou de um hackathon!
     </div>
-    <img class="hackathon-usp-logo" src="~/assets/logo-hackathonUSP.png">
+    <!-- <img class="hackathon-usp-logo" src="~/assets/logo-hackathonUSP.png"> -->
   </section>
 </template>
 
@@ -14,10 +14,23 @@
 .hackathon-usp-logo {
   width: 25%;
 }
-
 .hackathon-usp-info {
-  width: 60%;
-  font-size: 1.3em;
+  // width: 60%;
+  // font-size: 1.3em;
+  // text-align: justify;
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  font-size: 1.1em;
   text-align: justify;
+  height: 90vh;
+  padding: 10vh 2vh;
+  align-items: center;
+  // background configuration
+  background-image: url("~/assets/hackathonUSP.svg");
+  background-color: rgba(255, 255, 255, 0.5);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 90vh;
 }
 </style>
