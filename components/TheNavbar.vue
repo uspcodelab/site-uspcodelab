630cedb8ab1c1aea3454b36c152f4525bc5cb195
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-03-13 18:11:33 -0300

    Create new responsive navbar
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar">
+  <!-- <nav class="navbar">
     <a v-scroll-to="'#head'" class="nav-logo" href="#">
       <img src="~/assets/UCL-Logo.svg">
     </a>
@@ -10,7 +10,22 @@
         <div class="nav-line"/>
       </div>
     </div>
-  </nav>
+  </nav> -->
+  <b-navbar style="background-color: #ffffff" fixed="top" type="light" toggleable>
+    <b-navbar-brand v-scroll-to="'#head'">
+      <img src="~/assets/UCL-Logo_Text.svg" alt="BV" style="width: 60%; height: 40%; border-radius: 15px">
+    </b-navbar-brand>
+    <b-navbar-toggle target="nav_dropdown_collapse"/>
+    <b-collapse is-nav id="nav_dropdown_collapse">
+      <b-navbar-nav>
+        <b-nav-item href="#about" style="color: white;">Sobre</b-nav-item>
+        <b-nav-item href="#dev_journey">dev.journey()</b-nav-item>
+        <b-nav-item href="#CodeSchools">CodeSchools</b-nav-item>
+        <b-nav-item href="#HackathonUSP">HackathonUSP</b-nav-item>
+        <b-nav-item href="#footer" >Contato</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
@@ -31,85 +46,85 @@ export default {
 </script>
 
 <style lang="scss">
-.navbar {
-  color: $secondary-color;
+// .navbar {
+//   // color: $secondary-color;
 
-  min-width: 100%;
-  height: 8vh;
+//   min-width: 100%;
+//   height: 8vh;
 
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-between;
+//   display: flex;
+//   flex-flow: row nowrap;
+//   justify-content: space-between;
 
-  position: fixed;
-  top: 0px;
-  left: 0px;
-  z-index: 10;
+//   position: fixed;
+//   top: 0px;
+//   left: 0px;
+//   z-index: 10;
 
-  background-color: $terciary-color;
-  opacity: 0.95;
-}
+//   background-color: $terciary-color;
+//   opacity: 0.95;
+// }
 
-.nav-logo {
-  flex: 1 1;
-  opacity: 1;
-  margin: 0.8em;
+// .nav-logo {
+//   flex: 1 1;
+//   opacity: 1;
+//   margin: 0.8em;
 
-  img {
-    border-radius: 50%;
-  }
-}
+//   img {
+//     border-radius: 50%;
+//   }
+// }
 
-.nav-menu {
-  height: 100%;
+// .nav-menu {
+//   height: 100%;
 
-  margin-right: 1.2em;
+//   margin-right: 1.2em;
 
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-evenly;
-  align-items: center;
-  flex: 1 1 0;
-}
+//   display: flex;
+//   flex-flow: row nowrap;
+//   justify-content: space-evenly;
+//   align-items: center;
+//   flex: 1 1 0;
+// }
 
-.nav-button {
-  padding: 0 1em;
+// .nav-button {
+//   padding: 0 1em;
 
-  height: 100%;
+//   height: 100%;
 
-  background-size: 100% 200%;
+//   background-size: 100% 200%;
 
-  display: flex;
-  flex-flow: column wrap;
-  justify-content: center;
-  align-items: center;
-}
+//   display: flex;
+//   flex-flow: column wrap;
+//   justify-content: center;
+//   align-items: center;
+// }
 
-.nav-button:hover {
-  cursor: pointer;
-}
+// .nav-button:hover {
+//   cursor: pointer;
+// }
 
-.nav-line {
-  content: "";
-  display: block;
-  position: relative;
-  align-self: flex-start;
+// .nav-line {
+//   content: "";
+//   display: block;
+//   position: relative;
+//   align-self: flex-start;
 
-  margin-top: 5px;
-  width: 0;
-  height: 5px;
-  background: $secondary-color;
+//   margin-top: 5px;
+//   width: 0;
+//   height: 5px;
+//   background: $secondary-color;
 
-  transition: width 0.3s ease;
-}
+//   transition: width 0.3s ease;
+// }
 
-.nav-button:hover .nav-line {
-  width: 100%;
-}
+// .nav-button:hover .nav-line {
+//   width: 100%;
+// }
 
-.nav-text {
-  color: white;
-  text-decoration: none;
-  font-size: 1.6em;
-}
+// .nav-text {
+//   color: white;
+//   text-decoration: none;
+//   font-size: 1.6em;
+// }
 </style>
