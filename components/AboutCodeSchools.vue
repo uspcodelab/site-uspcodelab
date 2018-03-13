8000fad66e1032c6cf4f5065e610c33942530e3f
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-03-13 20:01:10 -0300

    CodeSchools component is responsive
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- components/AboutCodeSchools.vue
+++ components/AboutCodeSchools.vue
@@ -4,7 +4,7 @@
       <div class="section-title">
         <h3>CodeSchools</h3>
       </div>
-      Nossas escolas de períodos de férias tem como objetivo um grande projeto de desenvolvimento em equipe e com tecnologias modernas.
+      Nossas escolas de períodos de férias tem como objetivo um grande projeto de desenvolvimento em equipe e com tecnologias modernas. Uma verdadeira experiência de desenvolvimento!
     </div>
   </section>
 </template>
@@ -14,7 +14,25 @@
   font-size: 1.3em;
 }
 .codeschools-section {
-  background: $terciary-color;
-  color: $secondary-color;
+  // background: $terciary-color;
+  // color: $secondary-color;
+  // background-image: url("~/assets/UCL-Logo-Translucid.svg");
+  // background-color: rgba(255, 255, 255, 0.452);
+  // background-repeat: no-repeat;
+  // background-size: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  font-size: 1.1em;
+  text-align: justify;
+  height: 100vh;
+  padding: 10vh 2vh;
+  align-items: center;
+  // background configuration
+  background-image: url("~/assets/UCL-Logo-NoBackground.svg");
+  background-color: rgba(5, 5, 5, 0.7);
+  background-repeat: no-repeat;
+  background-origin: center;
+  background-size: cover;
 }
 </style>
