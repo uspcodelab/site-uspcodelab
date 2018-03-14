97a562f8a15f5703ce3abbfd1d9736ac01b80cb9
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-03-14 19:28:18 -0300

    Add dev.journey images
    Add correct dev.journey images and titles for each stage.
    
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- components/AboutDevJourney.vue
+++ components/AboutDevJourney.vue
@@ -1,10 +1,47 @@
 <template>
   <b-row>
-    <b-col sm class="dev-journey-info" v-for="obj in dev_section" :key="obj.KEY">
+    <!-- <b-col sm class="dev-journey-info" v-for="obj in dev_section" :key="obj.KEY">
       <div class="dev-journey-card-icon">
         <img src="~/assets/resume.svg">
       </div>
       <div>
+        <h4>dev.start()</h4>
+        Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?
+      </div>
+    </b-col> -->
+    <b-col sm class="dev-journey-info">
+      <div class="dev-journey-card-icon">
+        <img src="~/assets/play-button-sing.svg">
+      </div>
+      <div>
+        <h4>dev.start()</h4>
+        Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?
+      </div>
+    </b-col>
+    <b-col sm class="dev-journey-info">
+      <div class="dev-journey-card-icon">
+        <img src="~/assets/study.svg">
+      </div>
+      <div>
+        <h4>dev.learn()</h4>
+        Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?
+      </div>
+    </b-col>
+    <b-col sm class="dev-journey-info">
+      <div class="dev-journey-card-icon">
+        <img src="~/assets/startup.svg">
+      </div>
+      <div>
+        <h4>dev.boost()</h4>
+        Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?
+      </div>
+    </b-col>
+    <b-col sm class="dev-journey-info">
+      <div class="dev-journey-card-icon">
+        <img src="~/assets/resume.svg">
+      </div>
+      <div>
+        <h4>dev.hire()</h4>
         Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?
       </div>
     </b-col>
@@ -12,44 +49,44 @@
 </template>
 
 <script>
-export default {
-  data: () => {
-    return {
-      dev_section: [
-        {
-          KEY: 1,
-          icon: "play-button-sing.svg",
-          text:
-            "Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?"
-        },
-        {
-          KEY: 2,
-          icon: "study.svg",
-          text:
-            "Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?"
-        },
-        {
-          KEY: 3,
-          icon: "startup.svg",
-          text:
-            "Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?"
-        },
-        {
-          KEY: 4,
-          icon: "resume.svg",
-          text:
-            "Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?"
-        }
-      ]
-    };
-  },
-  computed: {
-    path(obj) {
-      alert(obj);
-      return "~/assets/" + "resume" + ".svg";
-    }
-  }
-};
+// export default {
+//   data: () => {
+//     return {
+//       dev_section: [
+//         {
+//           KEY: 1,
+//           icon: "play-button-sing.svg",
+//           text:
+//             "Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?"
+//         },
+//         {
+//           KEY: 2,
+//           icon: "study.svg",
+//           text:
+//             "Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?"
+//         },
+//         {
+//           KEY: 3,
+//           icon: "startup.svg",
+//           text:
+//             "Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?"
+//         },
+//         {
+//           KEY: 4,
+//           icon: "resume.svg",
+//           text:
+//             "Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?"
+//         }
+//       ]
+//     };
+//   },
+//   computed: {
+//     path(obj) {
+//       alert(obj);
+//       return "~/assets/" + "resume" + ".svg";
+//     }
+//   }
+// };
 </script>
 
 <style lang="scss">
