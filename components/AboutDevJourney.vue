b7e9f044be61f17b4c8febbd815220b54346fa17
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-03-14 20:56:44 -0300

    Add texts for each stage
    Add descriptions from our "Modelo de patrocínio" document into each
    section.
    
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- components/AboutDevJourney.vue
+++ components/AboutDevJourney.vue
@@ -15,7 +15,7 @@
       </div>
       <div>
         <h4>dev.start()</h4>
-        Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?
+        Ciclo de oficinas para apresentar programação web para o público geral da universidade, com tópicos como <em>HTML</em>, <em>CSS</em> e <em>JavaScript</em>. 
       </div>
     </b-col>
     <b-col sm class="dev-journey-info">
@@ -24,7 +24,7 @@
       </div>
       <div>
         <h4>dev.learn()</h4>
-        Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?
+        Um dos nossos grupos de estudo com reuniões semanais, ao longo de um semestre, para aplicar os conhecimentos em projetos propostos pela organização.
       </div>
     </b-col>
     <b-col sm class="dev-journey-info">
@@ -33,7 +33,7 @@
       </div>
       <div>
         <h4>dev.boost()</h4>
-        Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?
+        Uma continuação da fase anterior, agora com projetos em times, voltada para aprendizado de técnicas de modelagem, implementação e manutenção de sistemas.
       </div>
     </b-col>
     <b-col sm class="dev-journey-info">
@@ -42,7 +42,7 @@
       </div>
       <div>
         <h4>dev.hire()</h4>
-        Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?
+        Nosso programa de captação de currículos de alunos, em particular os participantes do programa, para facilitar o contato profissional com empresas parceiras.
       </div>
     </b-col>
   </b-row>
@@ -95,6 +95,7 @@
   color: $secondary-color;
 
   height: 90vh;
+  padding: 0 2vw;
 
   display: flex;
   flex-flow: row wrap;
