8ce2151ece5493549e21305f7c5fb5719277a99f
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-03-14 18:51:00 -0300

    Cleaning up some old code
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- components/AboutDevJourney.vue
+++ components/AboutDevJourney.vue
@@ -1,24 +1,13 @@
 <template>
-  <!-- <section id="dev_journey" class="dev-journey-info"> -->
-  <b-row id="dev_journey" bg-color>
-    <!-- <div class="section-title dev-journey-title">
-      <h3>dev.journey()</h3>
-    </div> -->
-    <!-- <div v-for="obj in dev_section" :key="obj.KEY" class="dev-journey-card"> -->
+  <b-row>
     <b-col sm class="dev-journey-info" v-for="obj in dev_section" :key="obj.KEY">
       <div class="dev-journey-card-icon">
-        <!-- <img src="../assets/study.svg"> -->
         <img src="~/assets/resume.svg">
       </div>
-      <!-- <div class="dev-journey-card-text">
-        {{ obj.text }}
-      </div> -->
       <div>
         Elit nisi consequuntur dolores tempore error! Voluptate aspernatur repellat minima molestias nostrum? Distinctio ullam neque voluptas quasi maiores. Officia laboriosam possimus in aspernatur tempore Provident reprehenderit excepturi voluptates quidem blanditiis?
       </div>
-    <!-- </div> -->
     </b-col>
-  <!-- </section> -->
   </b-row>
 </template>
 
@@ -64,14 +53,11 @@ export default {
 </script>
 
 <style lang="scss">
-.bg-color {
-  background-color: #ff690a;
-}
 .dev-journey-info {
   background: $main-color;
   color: $secondary-color;
 
-  min-height: 40vh;
+  height: 90vh;
 
   display: flex;
   flex-flow: row wrap;
--- components/AboutHackathonUsp.vue
+++ components/AboutHackathonUsp.vue
@@ -17,20 +17,18 @@
 .hackathon-usp-info {
   // width: 60%;
   // font-size: 1.3em;
-  // text-align: justify;
   display: flex;
   flex-wrap: wrap;
   width: 100%;
   font-size: 1.1em;
-  // text-align: justify;
   height: 90vh;
   padding: 10vh 2vh;
   align-items: center;
   // background configuration
-  background-image: url("~/assets/hackathonUSP.svg");
+  background-image: url("~/assets/NEWhackathonUSP.svg");
   background-color: rgba(255, 255, 255, 0.5);
   background-repeat: no-repeat;
   background-position: center;
-  background-size: 90vh;
+  // background-size: 90vh;
 }
 </style>
