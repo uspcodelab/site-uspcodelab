07d458588d91a62ca8ce85797cc558c6aaa95c54
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-03-14 18:50:10 -0300

    Code cleanup
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- components/AboutCodeLab.vue
+++ components/AboutCodeLab.vue
@@ -1,14 +1,11 @@
 <template>
   <section id="about" class="about">
-    <!-- <div class="about"> -->
     <div class="section-title">
       <h3>Quem somos?</h3>
     </div>
     <div id="about-text">
-      O USPCodeLab (UCL) é um grupo de extensão universitária com o objetivo  de criar um espaço colaborativo para o desenvolvimento de tecnologia na universidade. Para isso, atua em duas frentes: uma voltada para alunos de cursos de tecnologia (Ciência da Computação, Engenharia da Computação, Sistemas da Informação, etc.), com foco em ajudá-los a se tornarem engenheiros de software capazes de utilizar as melhores ferramentas e técnicas de desenvolvimento disponíveis; e outra para o público geral interessado, para difundir o uso de tecnologia para criação de soluções em suas respectivas áreas.
+      O USPCodeLab (UCL) é um grupo de extensão universitária com o objetivo  de criar um espaço colaborativo para o desenvolvimento de tecnologia na universidade, com nossos grupos de estudo, e oferecer aprendizado na área de Engenharia de Software.
     </div>
-    <!-- </div> -->
-    <!-- <img src="~/assets/UCL-Logo.svg" class="ucl-logo"> -->
   </section>
 </template>
 
