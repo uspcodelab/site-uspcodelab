b6341a59a1e2d632650e0bac2756d25fa80e1877
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-03-16 15:55:26 -0300

    New footer layout
    Add Google Maps embeded map inside footer.
    Change layout to better fit map.
    
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -4,18 +4,15 @@
       <b-col class="footer-left" style="text-align: center">
         <img src="~/assets/UCL-Logo_Footer.svg" style="border-radius: 10px;">
       </b-col>
-      <b-col class="footer-center">
-        <div>
-          <i class="fa fa-map-marker" style="font-size: 18px;"/>
-          <p><span>Rua do Matão, 1010 - Cidade Universitária</span> São Paulo, SP</p>
-        </div>
-
+      <!-- <b-col class="footer-center">
         <div>
           <i class="fa fa-envelope" style="font-size: 18px;"/>
           <p><a href="mailto:codelab@ime.usp.br">codelab@ime.usp.br</a></p>
+          <a href="https://www.facebook.com/uspcodelab/" class="facebook"><i class="fa fa-facebook"/></a>
+          <a href="https://www.gitlab.com/uspcodelab/" class="gitlab"><i class="fa fa-gitlab"/></a>
         </div>
-      </b-col>
-      <b-col class="footer-right">
+      </b-col> -->
+      <b-col class="footer-right" style="text-align: center">
         <p class="footer-company-about">
           <span>USPCodeLab</span>
         </p>
@@ -26,14 +23,26 @@
         </p>
       </b-col>
     </b-row>
-    <b-row class="footer-icons">
-      <a href="https://www.facebook.com/uspcodelab/"><i class="fa fa-facebook"/></a>
-      <a href="https://www.gitlab.com/uspcodelab/"><i class="fa fa-gitlab"/></a>
+    <b-row>
+      <iframe width="600" height="450" frameborder="0" class="map" src="https://www.google.com/maps/embed/v1/place?q=Rua%20do%20Mat%C3%A3o%2C%201010%20-%20Cidade%20Universit%C3%A1ria%20S%C3%A3o%20Paulo%2C%20SP&key=AIzaSyCZVWkSqO4NaGO350vPgR_GHZJr9GPp4ZM" allowfullscreen/>
+    </b-row>
+    <b-row class="footer-center">
+      <div>
+        <a href="mailto:codelab@ime.usp.br"><i class="fa fa-envelope"/></a>
+        <a href="https://www.facebook.com/uspcodelab/" class="facebook"><i class="fa fa-facebook"/></a>
+        <a href="https://www.gitlab.com/uspcodelab/" class="gitlab"><i class="fa fa-gitlab"/></a>
+      </div>
     </b-row>
   </footer>
 </template>
 
 <style lang="scss">
+.map {
+  height: 240px;
+  width: 100%;
+  margin-top: 30px;
+  margin-bottom: 20px;
+}
 .footer-distributed {
   background-color: #292c2f;
   color: #ffffff;
@@ -52,6 +61,7 @@
     font-size: 13px;
     font-weight: normal;
     margin: 0;
+    text-align: center;
 
     span {
       display: block;
@@ -79,6 +89,7 @@
     text-align: center;
   }
   .footer-links {
+    text-align: center;
     a {
       color: #ffffff;
       margin: 20px 0 12px;
@@ -87,21 +98,37 @@
     }
   }
   .footer-icons {
-    justify-content: center;
-    margin-top: 25px;
-    a {
-      width: 35px;
-      height: 35px;
-      background-color: #33383b;
-      cursor: pointer;
+    text-align: center;
+    padding-top: 30px;
+    padding-bottom: 38px;
+  }
+  .fa {
+    font-size: 30px;
+    margin-right: 15px;
+    margin-left: 20px;
+    background-color: white;
+    color: #182c39;
+    border-radius: 51%;
+    padding: 10px;
+    height: 50px;
+    width: 50px;
+    text-align: center;
+    line-height: 31px;
+    text-decoration: none;
+    transition: color 0.2s;
+  }
+  .fa-facebook:hover {
+    color: #2b55ff;
+  }
+  .fa-facebook:focus {
+    color: #2b55ff;
+  }
 
-      font-size: 20px;
-      color: #ffffff;
-      text-align: center;
-      line-height: 35px;
-      margin-right: 3px;
-      margin-bottom: 10px;
-    }
+  .fa-gitlab:hover {
+    color: #e24329;
+  }
+  .fa-gitlab:focus {
+    color: #e24329;
   }
 }
 </style>
