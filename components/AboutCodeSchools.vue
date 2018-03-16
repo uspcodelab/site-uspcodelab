d42a180d640847dbdcc741cc30fde2bf0cd40b6e
Vitor Silva <sirmochi45@gmail.com>
2018-03-16 16:46:34 -0300

    Added new image

--- components/AboutCodeSchools.vue
+++ components/AboutCodeSchools.vue
@@ -2,28 +2,19 @@
   <section id="CodeSchools" class="codeschools-section">
     <b-row>
       <b-col>
-        <h3 class="section-title">CodeSchools</h3>
+        <h3 class="section-title codeschools-padding">CodeSchools</h3>
       </b-col>
     </b-row>
     <b-row>
-      <b-col>
-        <p>
+      <b-col style="display:flex;">
+        <p class="codeschools-padding" style="align-self:flex-end; padding-bottom:1.2em;">
           Nossas escolas de períodos de férias tem como objetivo um grande projeto de desenvolvimento em equipe e com tecnologias modernas. Uma verdadeira experiência de desenvolvimento!
         </p>
       </b-col>
     </b-row>
     <b-row>
-      <b-col sm lg="3" md="4" xs="6" style="padding-top: 5vh;">
-        <img class="img-fluid" src="~/assets/summer-school1.jpeg" alt="">
-      </b-col>
-      <b-col sm lg="3" md="4" xs="6" style="padding-top: 5vh; padding-bottom: 5vh;">
-        <img class="img-fluid" src="~/assets/summer-school2-min.jpeg" alt="">
-      </b-col>
-      <b-col sm lg="3" md="4" xs="6" style="padding-top: 5vh; padding-bottom: 5vh;">
-        <img class="img-fluid" src="~/assets/summer-school3-min.jpeg" alt="">
-      </b-col>
-      <b-col sm lg="3" md="4" xs="6" style="padding-top: 5vh; padding-bottom: 5vh;">
-        <img class="img-fluid" src="~/assets/summer-school4-min.jpeg" alt="">
+      <b-col style="padding:0;">
+        <img class="img-fluid" src="~/assets/summer-schools0-grad.jpeg" alt="">
       </b-col>
     </b-row>
   </section>
@@ -39,15 +30,17 @@
   font-size: 1.1em;
   height: 100%;
   width: 100%;
-  padding: 20vh 2vh;
+  padding: 20vh 0vh 0vh 0vh;
   color: #ffffff;
   background-color: #2c2c2c;
+  object-fit: cover;
 
   img {
-    width: 100%;
-    height: 100%;
-    border: 5px solid #000000;
-    border-radius: 5px;
+    min-height: 50vh;
   }
+}
+
+.codeschools-padding {
+  padding-left: 2vw;
 }
 </style>
