35d0b81ca66162ff6f94e85e926109fcbf86b15b
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-03-16 09:56:44 -0300

    CodeSchools section layout
    Move images from Summer School 2018 to assets folder.
    CodeSchools section is responsive and looks good and consistent across
    multiple screen sizes.
    
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- components/AboutCodeSchools.vue
+++ components/AboutCodeSchools.vue
@@ -1,6 +1,5 @@
 <template>
   <section id="CodeSchools" class="codeschools-section">
-    <!-- <div class="codeschools-info"> -->
     <b-row>
       <b-col>
         <h3 class="section-title">CodeSchools</h3>
@@ -11,6 +10,20 @@
         <p>
           Nossas escolas de períodos de férias tem como objetivo um grande projeto de desenvolvimento em equipe e com tecnologias modernas. Uma verdadeira experiência de desenvolvimento!
         </p>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col sm lg="3" md="4" xs="6" style="padding-top: 5vh;">
+        <img class="img-fluid" src="~/assets/summer-school1.png" alt="">
+      </b-col>
+      <b-col sm lg="3" md="4" xs="6" style="padding-top: 5vh; padding-bottom: 5vh;">
+        <img class="img-fluid" src="~/assets/summer-school2.png" alt="">
+      </b-col>
+      <b-col sm lg="3" md="4" xs="6" style="padding-top: 5vh; padding-bottom: 5vh;">
+        <img class="img-fluid" src="~/assets/summer-school3.png" alt="">
+      </b-col>
+      <b-col sm lg="3" md="4" xs="6" style="padding-top: 5vh; padding-bottom: 5vh;">
+        <img class="img-fluid" src="~/assets/summer-school4.png" alt="">
       </b-col>
     </b-row>
   </section>
@@ -24,10 +37,17 @@
   display: flex;
   flex-wrap: wrap;
   font-size: 1.1em;
-  height: 100vh;
+  height: 100%;
   width: 100%;
   padding: 20vh 2vh;
   color: #ffffff;
   background-color: #2c2c2c;
+
+  img {
+    width: 310px;
+    height: 230px;
+    border: 5px solid #000000;
+    border-radius: 5px;
+  }
 }
 </style>
