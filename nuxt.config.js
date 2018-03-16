cd816c259b926cb016d38cfd99886959d5c713a0
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-03-16 02:14:14 -0300

    Add CodeLab favicon
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,16 +13,14 @@ module.exports = {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: pkg.description }
     ],
-    script: [
-      {
-        src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"
-      }
-    ],
+    script: [{}],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      // { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "icon", type: "image/x-icon", href: "/favi.png" },
       {
         rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css?family=Cabin|IBM+Plex+Sans+Condensed|Nunito|Open+Sans"
+        href:
+          "https://fonts.googleapis.com/css?family=Cabin|IBM+Plex+Sans+Condensed|Nunito|Open+Sans"
       }
     ]
   },
