9b9f1637ca6c02e087653177f7891f520f808347
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-05-25 16:38:38 -0300

    Create new team page and main component
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

---
+++ components/TheTeam.vue
@@ -0,0 +1,86 @@
+<template>
+  <b-row id="team" tag="section" class="team text-center px-3 px-sm-5 py-5">
+
+    <b-col sm="12" class="px-lg-5 py-5">
+      <h1 class="text-primary pb-3">Equipe</h1>
+    </b-col>
+
+    <b-col sm="4" class="team__profile py-3 py-md-0">
+      <img src="../assets/team/placeholder.png"
+           class="w-50 py-3"
+           style="border-radius: 50%;">
+      <strong>Team member</strong>
+      <small>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Ipsum totam ad perspiciatis! Accusantium odio optio, 
+        architecto quisquam eum perferendis impedit modi suscipit cum id.
+        Nulla sapiente a ab fugit vitae?
+      </small>
+    </b-col>
+
+    <b-col sm="4" class="team__profile py-3 py-md-0">
+      <img src="../assets/team/placeholder.png"
+           class="w-50 py-3"
+           style="border-radius: 50%;">
+      <strong>Team member</strong>
+      <small>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Ipsum totam ad perspiciatis! Accusantium odio optio, 
+        architecto quisquam eum perferendis impedit modi suscipit cum id.
+        Nulla sapiente a ab fugit vitae?
+      </small>
+    </b-col>
+
+    <b-col sm="4" class="team__profile py-3 py-md-0">
+      <img src="../assets/team/placeholder.png"
+           class="w-50 py-3"
+           style="border-radius: 50%;">
+      <strong>Team member</strong>
+      <small>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Ipsum totam ad perspiciatis! Accusantium odio optio, 
+        architecto quisquam eum perferendis impedit modi suscipit cum id.
+        Nulla sapiente a ab fugit vitae?
+      </small>
+    </b-col>
+
+    <b-col sm="4" offset-sm="2" class="team__profile py-3 py-md-0">
+      <img src="../assets/team/placeholder.png"
+           class="w-50 py-3"
+           style="border-radius: 50%;">
+      <strong>Team member</strong>
+      <small>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Ipsum totam ad perspiciatis! Accusantium odio optio, 
+        architecto quisquam eum perferendis impedit modi suscipit cum id.
+        Nulla sapiente a ab fugit vitae?
+      </small>
+    </b-col>
+
+    <b-col sm="4" class="team__profile py-5 py-md-0">
+      <img src="../assets/team/placeholder.png"
+           class="circle w-50 py-3"
+           style="border-radius: 50%;">
+      <strong>Team member</strong>
+      <small>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Ipsum totam ad perspiciatis! Accusantium odio optio, 
+        architecto quisquam eum perferendis impedit modi suscipit cum id.
+        Nulla sapiente a ab fugit vitae?
+      </small>
+    </b-col>
+  </b-row>
+</template>
+
+<style lang="scss">
+.team {
+  min-height: calc(100vh);
+  background-color: theme-color("light");
+
+  &__profile {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+</style>
---
+++ pages/team.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="page">
+    <the-navbar/>
+    <b-container fluid>
+      <main>
+        <the-team />
+      </main>
+      <the-footer/>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import TheHeader from "~/components/TheHeader.vue";
+import TheNavbar from "~/components/TheNavbar.vue";
+import TheFooter from "~/components/TheFooter.vue";
+import TheTeam from "~/components/TheTeam.vue";
+
+export default {
+  components: {
+    TheNavbar,
+    TheHeader,
+    TheFooter,
+    TheTeam
+  }
+};
+</script>
+
+<style lang="scss">
+html {
+  font-size: 14px;
+}
+
+.page {
+  padding-top: 4rem;
+}
+
+@include media-breakpoint-up(lg) {
+  html {
+    font-size: 16px;
+  }
+}
+
+@include media-breakpoint-up(xl) {
+  html {
+    font-size: 1.5vmax;
+  }
+}
+</style>
