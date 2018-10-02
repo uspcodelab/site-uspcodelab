9985754ba525b0fa756b3442bfa718f625a55b25
ricardokojo <ricardokojo7@gmail.com>
2018-10-02 14:59:58 -0300

    Add vue-carousel to dependencies, organize main.js
    Add vue-carousel to package.json dependencies
    Add vue-carousel as import at main.js
    Organize main.js by packages imported
    
    Signed-off-by: ricardokojo <ricardokojo7@gmail.com>

--- src/main.js
+++ src/main.js
@@ -5,13 +5,19 @@ import router from "./router";
 import "./registerServiceWorker";
 Vue.config.productionTip = false;
 
-// Import Dependencies
+// bootstrap-vue
 
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+Vue.use(BootstrapVue);
+
+// vue-scrollto
 
 const VueScrollTo = require("vue-scrollto");
+Vue.use(VueScrollTo);
+
+// FontsAwesome
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
@@ -25,10 +31,14 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 library.add(faEnvelope, faFacebook, faGithub, faGitlab, faMedium, faTelegram);
-
 Vue.component("font-awesome-icon", FontAwesomeIcon);
-Vue.use(BootstrapVue);
-Vue.use(VueScrollTo);
+
+// vue-carousel
+
+import VueCarousel from 'vue-carousel';
+Vue.use(VueCarousel);
+
+// Vue
 
 new Vue({
   router,
