ab998eafaedb7a4507fb59fdb0cf23663a89e387
ricardokojo <ricardokojo7@gmail.com>
2018-10-25 14:59:17 -0300

    Update AboutHackathonUsp to use Tailwind CSS
    Remove bootstrap-vue components at AboutHackathonUsp
    Update AboutHackathonUsp component to use Tailwind CSS
    
    Signed-off-by: ricardokojo <ricardokojo7@gmail.com>

--- src/components/AboutHackathonUsp.vue
+++ src/components/AboutHackathonUsp.vue
@@ -1,21 +1,20 @@
 <template>
-  <b-row id="hackathon-usp" tag="section" class="hackathon-usp px-3 py-5 px-sm-5">
-    <b-col sm="6" class="hackathon-usp__logo py-5 py-sm-0">
-      <img src="@/assets/logos/hackathon-usp.svg">
-    </b-col>
-
-    <b-col sm="6" class="hackathon-usp__text py-5 py-sm-0">
-      <h1 class="title text-center text-md-left pb-3">
+  <section id="hackathon-usp" class="hackathon-usp flex flex-wrap flex-row-reverse px-4 py-20">
+    <div class="w-full md:w-1/2 flex items-end md:items-center justify-center md:pb-4">
+      <img src="@/assets/logos/hackathon-usp.svg" alt="Logo do HackathonUSP" class="hackhathon-usp-logo pb-5">
+    </div>
+    <div class="w-full md:w-1/2 flex flex-col items-center md:items-start text-center md:text-left justify-center px-2 md:px-12">
+      <h1 class="title font-bold tracking-wide pb-3">
         HackathonUSP
       </h1>
-      <p class="text-center text-md-left">
+      <p class="lg:text-xs text-grey-darker">
         Maior evento do USPCodeLab, feito em parceria com o <a href="http://www.uspempreende.org">Núcleo de Empreendedorismo da USP (NEU)</a>. Voltado para USPianos,
         patrocinado pela reitoria da USP e empresas parceiras,
         focado em quem vai participar de um hackathon pela primeira vez!
       </p>
       <Button message="Saiba mais" link="https://uclab.xyz/hack" />
-    </b-col>
-  </b-row>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -42,40 +41,8 @@ export default {
     }
   }
 
-  &__logo {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: flex-start;
-
-    img {
-      max-height: 40vh;
-    }
-  }
-
-  &__text {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: flex-end;
-  }
-}
-
-@media (min-width: 768px) {
-  .hackathon-usp__logo {
-    order: 2;
-    justify-content: center;
-
-    &__img {
-      min-width: 0;
-      width: 90%;
-    }
-  }
-
-  .hackathon-usp__text {
-    order: 1;
-    align-items: left;
-    justify-content: center;
+  .hackhathon-usp-logo {
+    max-height: 40vh;
   }
 }
 </style>
