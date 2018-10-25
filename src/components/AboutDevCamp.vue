7c97ceb590a875fda3d96654bac713713adddda9
ricardokojo <ricardokojo7@gmail.com>
2018-10-25 15:58:13 -0300

    Update AboutDevCamp to use Tailwind CSS
    Remove bootstrap-vue at AboutDevCamp component
    Update AboutDevCamp to use Tailwind CSS
    
    Signed-off-by: ricardokojo <ricardokojo7@gmail.com>

--- src/components/AboutDevCamp.vue
+++ src/components/AboutDevCamp.vue
@@ -1,23 +1,22 @@
 <template>
-  <b-row id="dev-camp" tag="section" class="dev-camp px-3 py-5 px-sm-5">
-    <b-col sm="12" class="py-5">
-      <h1 class="title text-center pb-3">
-        dev.camp()
-      </h1>
-      <p class="text-center">
-        Escolas de férias voltadas ao desenvolvimento de projetos em equipe
-        usando as tecnologias mais modernas. Uma verdadeira experiência
-        de desenvolvimento!
-      </p>
-    </b-col>
-    <b-col sm="12" class="dev-camp__showcase text-center pb-5">
+  <section id="dev-camp" class="dev-camp px-4 py-20 text-center">
+    <h1 class="title font-bold tracking-wide pb-3">
+      dev.camp()
+    </h1>
+    <p class="lg:text-xs text-center pb-4">
+      Escolas de férias voltadas ao desenvolvimento de projetos em equipe
+      usando as tecnologias mais modernas. Uma verdadeira experiência
+      de desenvolvimento!
+    </p>
+    <div class="flex flex-col items-center">
       <carousel :autoplay="true"
-                :autoplay-timeout="3000"
-                :loop="true"
-                :pagination-enabled="true"
-                :pagination-active-color="'#f8f9fA'"
-                :pagination-color="'#505050'"
-                :per-page="1">
+              :autoplay-timeout="3000"
+              :loop="true"
+              :pagination-enabled="true"
+              :pagination-active-color="'#f8f9fA'"
+              :pagination-color="'#505050'"
+              :per-page="1"
+              class=" md:w-1/2">
         <slide>
           <img src="@/assets/dev-camp/team-process.jpg">
         </slide>
@@ -28,16 +27,8 @@
           <img src="@/assets/dev-camp/team-user.jpg">
         </slide>
       </carousel>
-      <!--
-      <b-carousel :interval="4000" class="dev-camp__showcase__carousel"
-                  controls indicators>
-        <b-carousel-slide img-src="@/assets/logos/ucl-logo-colored-alpha.svg"/>
-        <b-carousel-slide img-src="@/assets/dev-camp/team-request.jpg"/>
-        <b-carousel-slide img-src="@/assets/dev-camp/team-user.jpg"/>
-      </b-carousel>
-      -->
-    </b-col>
-  </b-row>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -55,34 +46,5 @@ export default {
 .dev-camp {
   color: $ucl-light;
   background-color: $ucl-black;
-
-  &__showcase {
-    display: flex;
-    justify-content: center;
-  }
-}
-
-.VueCarousel {
-  width: 80vw;
-  height: auto;
-}
-
-.VueCarousel-slide {
-  img {
-    width: 80vw;
-    height: auto;
-  }
-}
-
-@media (min-width: 992px) {
-  .VueCarousel {
-    width: 50vw;
-  }
-
-  .VueCarousel-slide {
-    img {
-      width: 50vw;
-    }
-  }
 }
 </style>
