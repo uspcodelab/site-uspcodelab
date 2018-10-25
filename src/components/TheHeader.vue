01a95a07b81a6190aee9ca791a6106f381d67670
ricardokojo <ricardokojo7@gmail.com>
2018-10-25 13:26:46 -0300

    Update TheHeader component to Tailwind CSS
    Remove bootstrap-vue components
    Add elements using Tailwind CSS
    
    Signed-off-by: ricardokojo <ricardokojo7@gmail.com>

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,25 +1,25 @@
 <template>
-  <b-row id="header" tag="section" class="header p-3 p-sm-5">
-    <b-col sm="6" class="header__logo pb-4 pb-sm-0">
-      <img src="@/assets/logos/ucl-mark-alpha.svg" class="header__logo__img rounded-circle">
-    </b-col>
-    <b-col sm="6" class="header__text">
-      <h1 class="text-center text-md-left pb-3 w-100">
+  <header class="header w-screen flex flex-wrap flex-row-reverse px-3 pt-5 md:pb-5 pb-footer">
+    <div class="w-full md:w-1/2 flex items-end md:items-center justify-center md:pb-4">
+      <img src="@/assets/logos/ucl-mark-alpha.svg" class="w-4/5 rounded-circle">
+    </div>
+    <div class="w-full md:w-1/2 flex flex-col items-center md:items-start text-center md:text-left justify-center px-4 md:px-12">
+      <h1 class="font-bold tracking-wide">
         USPCodeLab
       </h1>
-      <p class="lead text-center text-md-left">
+      <p class="text-xl">
         Grupo de extensão universitária que tem como objetivo
-        <strong>estimular a inovação tecnológica na USP</strong>
+        <b>estimular a inovação tecnológica na USP</b>
       </p>
-    </b-col>
-  </b-row>
+    </div>
+  </header>
 </template>
 
 <style lang="scss">
 .header {
   min-height: calc(100vh - 4rem);
 
-  color: theme-color("light");
+  color: $ucl-light;
   background-image: linear-gradient(#ff8b46, $ucl-orange);
 
   &__logo {
