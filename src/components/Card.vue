1fe0da0836a6da711b7dc3bed3fe3c41dea527bf
ricardokojo <ricardokojo7@gmail.com>
2018-11-05 18:57:12 -0200

    Update Card and CardList paddings and margins
    Decrease padding and margin between cards in CardList
    Increase margin-top and margin-bottom at Card
    
    Signed-off-by: ricardokojo <ricardokojo7@gmail.com>

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="rounded overflow-hidden shadow-md text-center h-full">
-    <img :src="data.imgUrl" :alt="data.imgAlt" class="w-1/2 lg:w-1/3 mt-4">
-    <div class="px-6 py-4">
+    <img :src="data.imgUrl" :alt="data.imgAlt" class="w-1/2 lg:w-1/3 mt-6">
+    <div class="px-6 pt-4 mb-6">
       <div class="font-bold text-3xl lg:text-base mb-2">{{ data.name }}</div>
-      <p class="text-grey-darker text-base lg:text-xs leading-normal mb-4">
+      <p class="text-grey-darker text-base lg:text-xs leading-normal">
         {{ data.description }}
       </p>
     </div>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex flex-wrap md:-mx-4 lg:px-32 pb-2">
-    <div v-for="data in array" :key="data.name" class="w-full md:w-1/2 md:px-4 lg:w-1/3 my-3">
+  <div class="flex flex-wrap md:-mx-2 lg:px-32 pb-2">
+    <div v-for="data in array" :key="data.name" class="w-full md:w-1/2 md:px-2 lg:w-1/3 my-2">
       <Card :data="data" />
     </div>
   </div>
