01e934683246e8b75f4774db9a8ea6dc9973afeb
ricardokojo <ricardokojo7@gmail.com>
2018-11-22 01:23:23 -0200

    Add alt attribute to USPCodeLab logo at TheHeader
    Fix accessability issue
    
    Signed-off-by: ricardokojo <ricardokojo7@gmail.com>

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header id="header" class="header h-screen md:h-full flex flex-wrap flex-row-reverse px-3 pt-5 pb-mobile md:pt-navbar md:pb-5">
     <div class="w-full md:w-1/2 flex items-end md:items-center justify-center md:pb-4">
-      <img src="@/assets/logos/ucl-mark-alpha.svg" class="w-4/5 md:w-3/5 rounded-full">
+      <img src="@/assets/logos/ucl-mark-alpha.svg" class="w-4/5 md:w-3/5 rounded-full" alt="Logo do USPCodeLab">
     </div>
     <div class="w-full md:w-1/2 flex flex-col items-center md:items-start text-center md:text-left justify-center px-4 md:px-12">
       <h1 class="text-4xl font-bold tracking-wide">
