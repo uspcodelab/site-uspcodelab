9d8a1250d5b92fce1ba2865213eb70b5dca75d2d
Ricardo H. H. Kojo <ricardokojo7@gmail.com>
2019-01-28 01:41:01 -0200

    Add v-scroll-to directives at MobileMenu and TheNavbar router-links
    Signed-off-by: Ricardo H. H. Kojo <ricardokojo7@gmail.com>

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -3,10 +3,8 @@
     <div v-scroll-to="'#header'" class="flex items-center flex-no-shrink mr-4 cursor-pointer">
       <img src="@/assets/logos/ucl-logo-colored-alpha.svg" alt="USPCodeLab" class="h-10">
     </div>
-    <router-link v-for="link in links" :to="link.url" :key="link.name" tag="div" class="mr-2">
-      <a class="text-grey-dark hover:text-grey-darkest text-xs no-underline select-none mr-4 ">
-        {{ link.name }}
-      </a>
+    <router-link v-scroll-to="'#header'" v-for="link in links" :to="link.url" :key="link.name" class="text-grey-dark hover:text-grey-darkest text-xs cursor-pointer select-none no-underline mr-4">
+      {{ link.name }}
     </router-link>
   </nav>
 </template>
