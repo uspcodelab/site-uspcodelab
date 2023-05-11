92196a4f0d9c6a41feedb61c8d9637feb0fa143f
João Paulo Pereira <jppaulo11@usp.br>
2023-05-10 22:21:19 -0300

    Make the codelab bits social medias iterative inside CodelabBits.vue
    Besides that, adds an original image to Instagram card.

--- src/components/dev-learn/CodelabBits.vue
+++ src/components/dev-learn/CodelabBits.vue
@@ -3,56 +3,71 @@
     <div class="container mx-auto flex flex-wrap flex-col leading-loose">
       <div class="mb-10 md:mb-20 tracking-wide">
         <p class="text-ucl-orange mb-8 text-center text-5xl">
-          Codelab
-          <span>
-            <span style="color: #ff34d2">B</span>
-            <span style="color: #05ff00">i</span>
-            <span style="color: #24ffff">t</span>
-            <span style="color: #ff0000">s</span>
-          </span>
+        Codelab
+        <span>
+          <span style="color: #ff34d2">B</span>
+          <span style="color: #05ff00">i</span>
+          <span style="color: #24ffff">t</span>
+          <span style="color: #ff0000">s</span>
+        </span>
         </p>
         <span>
           Cursos sobre tecnologia onde os participantes são introduzidos a
           ferramentas e técnicas de desenvolvimento introdutórias e avançadas.
         </span>
       </div>
-      <div class="flex flex-wrap justify-between mb-20">
+      <div v-for="(media, i) in medias" :key="media.title" class="flex flex-wrap justify-between mb-20" :class="{ 'flex-row-reverse': i % 2}">
         <div class="text-center md:text-left w-full md:w-9/20 mb-4 md:mb-0">
-          <h2 class="font-light mb-5 md:mb-0" style="color: #ff0000">
-            Youtube
+          <h2 class="font-light mb-5 md:mb-0" :style="{ color: media.titleColor }">
+            {{ media.title }}
           </h2>
           <span>
-            Desde 2019, administramos o canal do youtube USPCodelab. Postamos
-            vídeos educativos sobre computação. Feito por alunos, os vídeos
-            abrangem desde cursos introdutórios até técnicas de programação mais
-            avançadas.
+            {{ media.description }}
           </span>
         </div>
         <img
-          src="@/assets/dev-learn/youtube.png"
-          class="w-full md:w-1/2 h-full md:h-1/2"
+        :src="media.src"
+        class="w-full h-full md:w-1/2  md:h-1/2"
         >
-      </div>
-      <div class="flex flex-wrap flex-wrap-reverse justify-between">
-        <img
-          src="@/assets/dev-learn/youtube.png"
-          class="w-full md:w-1/2 h-full md:h-1/2"
-        >
-        <div class="text-center md:text-left w-full md:w-9/20 mb-4 md:mb-0">
-          <h2 class="font-fin mb-5 md:mb-0" style="color: #ff34d2">
-            Instagram
-          </h2>
-          <span>
-            Lorem ipsum dolor sit ametesse ea nulla sunt ex occaecat
-            reprehenderit commodo officia dolor Lorem duis laboris cupidatat
-            officia voluptate. Culpa proident adipisicing id nulla nisi laboris
-            ex in Lorem sunt duis officia eiusmod. Aliqua duis
-          </span>
-        </div>
       </div>
     </div>
   </section>
 </template>
+
+<script>
+
+import YoutubeImage from '@/assets/dev-learn/youtube.png'
+import InstagramImage from '@/assets/dev-learn/instagram.png'
+  export default {
+    data() {
+      return {
+        medias: [
+          {
+            title: "Youtube",
+            titleColor: "#ff0000",
+            description: `
+            Desde 2019, administramos o canal do youtube USPCodelab. Postamos
+            vídeos educativos sobre computação. Feito por alunos, os vídeos
+            abrangem desde cursos introdutórios até técnicas de programação mais
+            avançadas.`,
+            src: YoutubeImage
+          },
+          {
+            title: "Instagram",
+            titleColor: "#ff34d2",
+            description: `
+            Lorem ipsum dolor sit ametesse ea nulla sunt ex occaecat
+            reprehenderit commodo officia dolor Lorem duis laboris cupidatat
+            officia voluptate. Culpa proident adipisicing id nulla nisi laboris
+            ex in Lorem sunt duis officia eiusmod. Aliqua duis
+            `,
+            src: InstagramImage
+          }
+        ]
+      }
+    }
+  }
+</script>
 
 <style scoped>
 img {
